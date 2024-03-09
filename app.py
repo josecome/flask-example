3ba329084e872f924234ae71c95dae228debfdd3
Jose Jaime Come <josejaimecome@gmail.com>
2024-03-09 18:13:12 +0200

    modules folder added and other changes made

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/")
-def home():
-    error = None
-    return render_template('home.html', error=error)
-
-@app.route("/<page>")
-def page(page):
-    return f"You are in page: {escape(page)}!"
+import modules

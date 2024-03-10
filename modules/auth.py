9df83a2ed5360c9dcd0e3692ddf226b0deb5fd14
Jose Jaime Come <josejaimecome@gmail.com>
2024-03-10 14:07:35 +0200

    auth.py file added and other minor changes made

---
+++ modules/auth.py
@@ -0,0 +1,22 @@
+from flask import request, redirect, url_for, session
+from app import app
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('home'))
--- modules/index.py
+++ modules/index.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, session
 from app import app
 
 # Route for Home page
 @app.route("/")
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return render_template('home.html')    
+    return "You are not logged in, <a href='/login'>Login</a>"
+    

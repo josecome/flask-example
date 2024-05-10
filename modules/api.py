23262ab7d1e48b7b21891afa453aaa4540901f6f
Jose Jaime Come <josejaimecome@gmail.com>
2024-05-10 21:58:00 +0200

    Jwt auth added

--- modules/api.py
+++ modules/api.py
@@ -1,5 +1,42 @@
 from app import app
 from flask import jsonify
+from flask_jwt import JWT, jwt_required, current_identity
+
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'user1', 'abcxyz'),
+    User(2, 'user2', 'abcxyz'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user: # and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/api/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 @app.route("/api")
 @app.route("/api/<post>")
